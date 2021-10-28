b1a0b95be16024cb984f5c4b5b7d8cfa4021025c
MVishukumar <beingvishu@gmail.com>
2021-10-28 22:57:33 +0530

    State Machine Enumerations.

---
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderEventEnum.java
@@ -0,0 +1,7 @@
+package guru.sfg.beer.order.service.domain;
+
+public enum BeerOrderEventEnum {
+  E_VALIDATE_ORDER, E_VALIDATION_PASSED, E_VALIDATION_FAILED,
+  E_ALLOCATION_SUCCESS, E_ALLOCATION_NO_INVENTORY, E_ALLOCATION_FAILED,
+  E_BEER_ORDER_PICKED_UP
+}
