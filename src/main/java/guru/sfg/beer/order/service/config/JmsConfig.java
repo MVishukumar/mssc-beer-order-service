8049fe03786a4f50a54476ac022699dc0a866e29
MVishukumar <beingvishu@gmail.com>
2021-10-31 23:26:32 +0530

    Send Validate Order Request with Action.

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class JmsConfig {
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
 
     // Serialize message content to json using TextMessage
     @Bean
